8a36e9c9aa9e7027de9eafbb8de71e396dea56ff
Victoria Forrest <Elizatoria@gmail.com>
2024-02-15 00:57:54 -0500

    Class 2

---
+++ Class2/variables_operators.py
@@ -0,0 +1,147 @@
+""" Addition """
+# num1 = 8
+# num2 = 4
+# print(num1 + num2)
+
+""" Subtraction """
+# print(num1 - num2)
+
+""" Multiplication """
+# print(num1 * num2)
+
+""" Division """
+# print(num1 / num2)
+
+""" Exponents """
+# print(num1 ** num2)
+
+""" Integer Division """
+# print(num1 // num2)
+
+""" Modulo/Mod/Remainder """
+# print(num1 % num2)
+
+""" Program to find the perimeter of a rectangle
+perimeter = (length + width) * 2 """
+Length = 8
+Width = 6
+Perimeter = (Length + Width) * 2
+print(Perimeter)
+
+
+""" Data Types """
+
+# # Integer
+# NumInt = 5
+# print(NumInt)
+# print(type(NumInt))
+
+# # String
+# FavColor = 'blue'
+# print(FavColor)
+# print(type(FavColor))
+
+# # Bool
+# TechErrors = False
+# print(TechErrors)
+# print(type(TechErrors))
+
+# # Float
+# NumFloat = 1.22
+# print(NumFloat)
+# print(type(NumFloat))
+
+# # String
+# FirstName = 'Jean'
+# LastName = 'Juste'
+# FullName = FirstName + ' ' + LastName
+# print(FullName)
+# print(FirstName, LastName)
+
+# # List 
+# StudentGrades = [100, 95, 70, 85, 40]
+# print(StudentGrades)
+# print(type(StudentGrades))
+
+# # For loop
+# for s in StudentGrades:
+#     print(s)
+
+
+# # Dictionary
+# DemographicInfo = {"First Name": "Jean",
+#                     "Last Name": "Juste",
+#                     "State": "New York"}
+
+# print(DemographicInfo)
+# print(type(DemographicInfo))
+
+
+# # Cast a string to an integer
+# MyString = '5'
+# print(type(MyString))
+# NewNumber = int(MyString) # casting our string to an integer
+# print(NewNumber)
+# print(type(NewNumber))
+
+
+# # Cast integer to string
+# SecondNum = 10
+# print(type(SecondNum))
+# NewString = str(SecondNum)
+# print(NewString)
+# print(type(NewString))
+
+
+# Student grade dataset
+
+
+# finding amount of assignments
+
+
+# Perimeter of a rectangle
+
+
+# Determine perimeter and display output
+
+
+# Fahrenheit to Celsius
+# F = 89
+# C = 5/9 * (F - 32)
+# print(C)
+
+
+# Eval
+coldWeather = 'True'
+print(eval(coldWeather))
+
+
+'''
+# You are given a triangle with a side #1 of 4, base of 6, and side #2 of 3. Create
+a brief python script that will determine the perimeter of the triangle. Comment your code
+
+1. Print the perimeter
+2. Using boolean operators is side #1 greater than the base?
+3. Using boolean operators is side #2 less than the base?
+4. Using boolean operators is base larger than or equal to side #1?
+'''
+
+# #Sides of the Triangle
+# Side1 = 4
+# Base = 6
+# Side2 =3
+
+# #Formula to calculate the Perimeter of the Triangle
+# Perimeter = Side1 + Base + Side2
+# print(Perimeter)
+
+# print(Side1 > Base)
+# print(Side2 < Base)
+# print(Base > Side1)
+
+
+
+
+
+
+
