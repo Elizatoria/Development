e8692186fedc44b64c621bddf2907d353951249b
Victoria Forrest <Elizatoria@gmail.com>
2024-02-21 22:56:28 -0500

    Final Push for Class 3

--- Class3/Pandas Preview/animals.py
+++ Class3/Pandas Preview/animals.py
@@ -4,6 +4,6 @@ animals = ['Lions', 'Tigers', 'Bears', 'Dogs', 'Cats']  # List Collection
 
 df = pd.DataFrame(animals)  # Covert our List to a Format to which Pandas can use
 
-print(df)
+# print(df)
 
-# df.to_csv('output.csv')  # This will give us our Output
+df.to_csv('output.csv')  # This will give us our Output
