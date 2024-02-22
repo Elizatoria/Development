d939ce22af24ba11b96ab507cad580af241ed16f
Victoria Forrest <Elizatoria@gmail.com>
2024-02-21 22:55:35 -0500

    Final Push for Class 3

--- Class3/Pandas Preview/animals.py
+++ Class3/Pandas Preview/animals.py
@@ -1,9 +1,9 @@
-import pandas as pd  # Import for Pandas Library
+import pandas as pd  # Import for Pandas Library  # pip install pandas
 
 animals = ['Lions', 'Tigers', 'Bears', 'Dogs', 'Cats']  # List Collection
 
 df = pd.DataFrame(animals)  # Covert our List to a Format to which Pandas can use
 
-# print(df)
+print(df)
 
-df.to_csv('output.csv')  # This will give us our Output
+# df.to_csv('output.csv')  # This will give us our Output
