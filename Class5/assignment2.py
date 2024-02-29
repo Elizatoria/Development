79346d5edc0b5f17dba4d066895befa09f271c1d
Victoria Forrest <Elizatoria@gmail.com>
2024-02-29 14:55:03 -0500

    Finish Push from Last Night

--- Class5/assignment2.py
+++ Class5/assignment2.py
@@ -23,7 +23,7 @@ test_1 = (clean_email[-4] == '.')
 # print(f'Test 1: Does {clean_email} have a "." at the third-to-last index?', test_1)
 
 # Test 2: It has exactly one "@" symbol, at the fifth-to-last index or earlier
-test_2 = (clean_email[-6] == '@')
+test_2 = (clean_email[-5] == '@')
 print(f'Does it have exactly one "@" symbol, at the fifth-to-last index or earlier?', test_2)
 
 # Test 3: There is at least one character before the "@" symbol
