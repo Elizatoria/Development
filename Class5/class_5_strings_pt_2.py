352cd9afa6e715934cabef29c38f935beb3ffea3
Victoria Forrest <Elizatoria@gmail.com>
2024-03-06 22:07:45 -0500

    Finished Class 5

--- Class5/class_5_strings_pt_2.py
+++ Class5/class_5_strings_pt_2.py
@@ -180,28 +180,37 @@ print("You entered",userin)
 How can you add sep and end keywords to get the exact formatting shown above?
 '''
 # userin = input("Please enter your input: ")
+# # print(f'You entered: {userin}.')  #How I would format it
+
+# # End Keyword Exercise
+# print("You entered:", end=' ')
+# print(userin, end='')
+# print('.', end='\n')
+
 
 # Formatted strings
 
 long_num = 12.34567890
+
 
 '''
 You need to write a script that will generate an email to a customer who has just made a purchase. You have 3 variables:
 name, which stores the customer's name as a string
 product, which stores the product name as a string
 price, which stores the price of the product as a float
-Use an f-string to generate an email message with the following text, and print it. Make sure to round the price to 2 decimal places. The email should be one multi-line string.
+Use an f-string to generate an email message with the following text, and print it. 
+Make sure to round the price to 2 decimal places. The email should be one multi-line string.
 Dear {name},
 Thank you for your purchase of a {product}. Your credit card has been charged ${price}.
 We appreciate your business and look forward to serving you again.
 Sincerely,
 The ABC Company
 '''
-
 name = 'Josiah Wilson'
 product = 'ABC Sneakers'
 price = 74.95343452342
 
+# Answer in Solutions
 
 
 '''
@@ -212,3 +221,4 @@ Examples:
 racecar: True
 cat: False
 '''
+# Answer in Solutions
