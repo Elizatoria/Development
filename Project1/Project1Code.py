d711d8194c7b43f239e79b6deaeb7203e5ce8a7b
Victoria Forrest <Elizatoria@gmail.com>
2024-03-17 04:00:29 -0400

    Working on Project One

--- Project1/Project1Code.py
+++ Project1/Project1Code.py
@@ -1,3 +1,15 @@
 taken_usernames = ['admin', 'admin123', 'superuser', 'superuser123']
 username, password = '', ''
 username_input, password_input = '', ''
+
+print('''To sign up, create a Username and Password.
+The Username must start with a lowercase letter and only contain letters, numbers, and underscores.
+The Password must contain, at least, each of the following.
+    ● 8 characters long
+    ● One uppercase letter
+    ● One lowercase letter
+    ● One digit
+    ● One of these characters:  !, ?, @, #, $, ^, &, *, _,  -
+    ● No spaces''')
+
+# while True:
