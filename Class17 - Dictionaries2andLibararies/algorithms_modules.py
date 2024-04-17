4242dc0d3fce7a6bba2fda2c7980d46ff598e3c7
Victoria Forrest <Elizatoria@gmail.com>
2024-04-17 19:48:05 -0400

    Libraries

---
+++ Class17 - Dictionaries2andLibararies/algorithms_modules.py
@@ -0,0 +1,5 @@
+print('Imported Algorithms Modules')
+
+def find_even(num):
+    '''Return True if Even, False if Odd'''
+    return True if num % 2 == 0 else False
---
+++ Class17 - Dictionaries2andLibararies/test_module_complete.py
@@ -0,0 +1,7 @@
+import algorithms_modules
+
+'''Module will return Boolean for Even or Odd'''
+
+num = 8
+result = algorithms_modules.find_even(num)
+print(result)
