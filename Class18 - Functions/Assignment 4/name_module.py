4ce187b18aa2491df6f94d15691e6a12a16541e4
Victoria Forrest <Elizatoria@gmail.com>
2024-04-24 14:56:22 -0400

    Working on Assignment 4

---
+++ Class18 - Functions/Assignment 4/name_module.py
@@ -0,0 +1,24 @@
+'''
+1. Create a python file called name_module.py. 
+2. Inside this python file, you will create 3 functions. 
+3. One called full_name, another called reverse_name, and a third called get_initials. 
+4. Each function will take 2 strings. 
+5. One string will be the first name, the second string will be the last name. 
+6. full_name will concatenate and return the full name. 
+For example if the first string is "Joseph" and the second string is "Simpson" This function will return the string, "Joseph Simpson". 
+Reverse name will flip the name around. 
+The name Diana Prince, would come back as Prince Diana. 
+The third function will take the first and last name and return the initials. 
+The name Tony Stark will return T.S.'''
+
+first_name = ''
+last_name = ''
+
+def full_name():
+    pass
+
+def reverse_name():
+    pass
+
+def get_initials():
+    pass
