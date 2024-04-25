c9c937318c50a8132403c93cb8864352dd6b9741
Victoria Forrest <Elizatoria@gmail.com>
2024-04-25 02:14:21 -0400

    Class 19

---
+++ Class19 - Functions2/Sample Assignment 4/main.py
@@ -0,0 +1,5 @@
+# This will contain my import statement and module calls.
+import my_module
+
+my_module.add_two(10, 20)
+my_module.say_hello('Brenetta')
---
+++ Class19 - Functions2/Sample Assignment 4/my_module.py
@@ -0,0 +1,9 @@
+# This will have my Functions.
+
+print('Import Successful')
+
+def say_hello(name):
+    print(f'Hello, {name}!')
+
+def add_two(x, y):
+    print(x + y)
